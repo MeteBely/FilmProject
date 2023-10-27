4cc2d51ba3b6bcee7a0c2e3b35c0b484891c53d5
MeteBely <metehanmuradoglu17@gmail.com>
2023-10-28 00:47:12 +0300

    Popüler dizilerin api'den çekilmesi.
    Popüler dizilerin api'den çekilip app.jsx'e gönderilme işlemi.

---
+++ FilmHelper/Api/PopulerTVSeries.jsx
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const PopulerTVSeriesData = async () => {
+  const response = await axios.get('https://api.themoviedb.org/3/discover/tv?include_adult=false&language=en-US&page=1&sort_by=popularity.desc', {
+    method: 'GET',
+    headers:{
+      accept: 'application/json',
+      Authorization: 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDhiOGJjN2IxMWFhMjE4ZTc4MWIxYjg0MTk3M2U5YyIsInN1YiI6IjY1MmQ1NGI1ZWE4NGM3MDEwYzFjZjVmYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3sva1b2fGlFXV0ewgaf0cbVAsWfV0s-5R0-HIwFaEoA'
+    },
+    params:{
+        language:'en-US',
+        page: 1
+      }
+  })
+  return response.data.results;
+}
+
+export default PopulerTVSeriesData
